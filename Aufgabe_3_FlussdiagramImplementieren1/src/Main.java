75766db897f8fe089eedd10259f2905e5743586f
Samuel_Ming <samuelming09@gmail.com>
2025-09-26 14:43:10 +0200

    Aufgabe 3 fertig

---
+++ Aufgabe_3_FlussdiagramImplementieren1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte i = 1;
+
+        while (i <= 10) {
+            System.out.println(i);
+            i++; // i = i + 1 geht auch, i++; ist simpler
+        }
+
+    }
+}
+
