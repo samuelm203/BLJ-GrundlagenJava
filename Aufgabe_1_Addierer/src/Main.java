68050c315a69acc82e997e4cb280db1f96fa0c91
Samuel_Ming <samuelming09@gmail.com>
2025-09-26 13:51:01 +0200

    Aufgabe 1 fertig

--- Aufgabe_1_Addierer/src/Main.java
+++ Aufgabe_1_Addierer/src/Main.java
@@ -1,7 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int zahlEins;
+        int zahlZwei;
+
+        System.out.println("Diese Programm berechnet die Summe von zwei Zahlen.");
+
+        System.out.print("Geben Sie ihre erste Zahl ein: ");
+        zahlEins = input.nextInt();
+
+        System.out.print("Geben Sie ihre zweite Zahl ein: ");
+        zahlZwei = input.nextInt();
+
+        /* Option 1: neuer Int der die Summe ausgeben würde
+        int summe = zahlEins + zahlZwei;
+        System.out.println("Die Summe der beiden Zahlen beträgt:" + summe); */
+
+        //Option 2: die Summe wird direkt in der Ausgabe ausgerechnet
+        System.out.println("Die Summe der beiden Zahlen beträgt: " + (zahlEins + zahlZwei) );
 
     }
 }
