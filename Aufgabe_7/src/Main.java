2f2fd5a433dc08cae495f04d3e707da0bdfe4ccc
Samuel_Ming <samuelming09@gmail.com>
2025-09-29 08:53:59 +0200

    Initial Commit

---
+++ Aufgabe_7/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Zahlen zwischen 1 und 30, die durch 5 und/oder 3, ohne Rest teilbar sind:");
+
+        int i = 1;
+        int ohneRestDurchDrei;
+        int ohneRestDurchFuenf;
+
+        while (i <= 30) {
+            ohneRestDurchDrei = i % 3;
+            ohneRestDurchFuenf = i % 5;
+
+            if (i == 30) {
+                System.out.println(i);
+                return;
+            }
+
+            if (ohneRestDurchDrei == 0 || ohneRestDurchFuenf == 0) {
+                System.out.print(i + ", ");
+            }
+
+            i++;
+        }
+
+
+    }
+}
