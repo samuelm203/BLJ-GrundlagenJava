299edfaa326efbb2a90135e0f51c15fd4b96a4bb
Samuel_Ming <samuelming09@gmail.com>
2025-09-29 11:40:50 +0200

    Initial Commit

--- zAufgabe_10/src/Main.java
+++ zAufgabe_10/src/Main.java
@@ -4,5 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
+        int zahl;
+        int quersumme = 0;
+
+        System.out.println("Gib eine ganzzahlige Zahl ein: ");
+        zahl = userInput.nextInt();
+
+
+
+        System.out.println("Die Quersumme von " + zahl + " ist " + berechneQuersumme(zahl, quersumme) + ".");
+    }
+
+    static int berechneQuersumme(int zahl, int quersumme) {
+        quersumme = 0;
+
+        while (zahl != 0) {
+            quersumme = quersumme + (zahl % 10);
+            zahl = zahl / 10;
+        }
+        return quersumme;
     }
 }
