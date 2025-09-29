a1db9a8e03934e4c3428ddf3e6eb29449db70e79
Samuel_Ming <samuelming09@gmail.com>
2025-09-29 08:33:36 +0200

    Aufgabe 6 fertig

--- Aufgabe_6_Kleines1mal1/src/Main.java
+++ Aufgabe_6_Kleines1mal1/src/Main.java
@@ -4,7 +4,22 @@ public class Main {
         System.out.println("Kleines 1x1");
         System.out.println("-----------");
 
-        System.out.println("hello world");
+        int i = 0;
+
+        while (i < 10) {
+            i++;
+            System.out.println(
+                    i * 1 + "\t" + "\t"
+                    + i * 2 + "\t" + "\t"
+                    + i * 3 + "\t" + "\t"
+                    + i * 4 + "\t" + "\t"
+                    + i * 5 + "\t" + "\t"
+                    + i * 6 + "\t" + "\t"
+                    + i * 7 + "\t" + "\t"
+                    + i * 8 + "\t" + "\t"
+                    + i * 9 + "\t" + "\t"
+                    + i * 10 + "\t" + "\t");
+        }
 
 
     }
