1824961b37a277e11d03f161d3faf81119d8f4c3
Samuel_Ming <samuelming09@gmail.com>
2025-10-02 08:39:22 +0200

    Initial Commit

---
+++ zAufgabe_19/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            int zahlEins = 0;
+            int zahlZwei = 0;
+            String operator = "";
+
+            System.out.println("Make your calculation (or press Q to quit): ");
+            String calculation = input.nextLine();
+
+            if (calculation.equals("Q") || calculation.equals("q")) {
+                System.out.println("The console will exit\n");
+                System.exit(0);
+            }
+
+            String[] rechnungInEinzelnTeilen = calculation.trim().split("\\s+");
+
+            for (int i = 0; i < rechnungInEinzelnTeilen.length; i++) {
+                if (rechnungInEinzelnTeilen[i].equals("/") || rechnungInEinzelnTeilen[i].equals("*") || rechnungInEinzelnTeilen[i].equals("-")|| rechnungInEinzelnTeilen[i].equals("+")) {
+                    operator =  rechnungInEinzelnTeilen[i];
+                } else if (i == 0) {
+                    zahlEins = Integer.parseInt(rechnungInEinzelnTeilen[0]);
+                } else {
+                    zahlZwei =  Integer.parseInt(rechnungInEinzelnTeilen[2]);
+                }
+            }
+
+            if (operator.equals("/")) {
+                System.out.println(zahlEins /  zahlZwei);
+            } else if (operator.equals("*")) {
+                System.out.println(zahlEins *  zahlZwei);
+            } else if (operator.equals("-")) {
+                System.out.println(zahlEins -  zahlZwei);
+            }  else {
+                System.out.println(zahlEins +  zahlZwei);
+            }
+        }
+    }
+}
